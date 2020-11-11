381caf21ba845a8cb88c91bcd9cb9e189e905884
Akshay Tekam <akshaytekam@atlassian.com>
2020-11-11 15:48:27 +0530

    Second Commit

--- Birthday_Month.sh
+++ Birthday_Month.sh
@@ -1,19 +1,16 @@
 #!/bin/bash -x
 
-declare -d dictionary
-MAXCOUNT=50
+declare -A birthDay  #Dictionary
+echo "${birthDay[@]}"
 
-echo -n "Enter Year of Birth"
-read BirthYear
-if [[ $BirthYear -eq 1992 ]]
-then
-while [[ "$count" -le $MAXCOUNT ]];
+index=1  #Variable
+	while [ $index -le 50 ]
 do
-month[$count]=$(($RANDOM%13))
-	let "count += 1"
+	random=$((RANDOM%12+1))
+	birthDay[$random]=$((birthDay[$random]+1))
+	((index++))
 done
 
-	echo "${month[*]}"
-else
-	echo "Age is not matched with Year"
-fi
+echo "Individuals Having Birthdays in the same Month."
+echo "${birthDay[@]}"
+
